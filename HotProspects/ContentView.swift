a828c1d85ab394200e9e51ded71da6d451a5ef5d
Jacek Kosinski <kashiash@gmail.com>
2023-11-12 15:44:36 +0100

    environment tests

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import Foundation
 
+@MainActor class User: ObservableObject {
+    @Published var name = "Jurek Ogurek"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        TextField("Name", text: $user.name).border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        Text(user.name)
+    }
+}
+struct EditView2: View  {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        TextField("Name:",text: $user.name)
+    }
+}
+
+struct DisplayView2: View  {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        Text(user.name)
+    }
+}
 struct ContentView: View {
+    @StateObject private var user = User()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            EditView()
+            DisplayView()
         }
         .padding()
+        .environmentObject(user)
     }
 }
 
